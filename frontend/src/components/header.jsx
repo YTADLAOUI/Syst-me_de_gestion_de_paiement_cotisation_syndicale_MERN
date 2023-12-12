861cf097785a636976f1d26d9979f68552975ac0
youssef tadlaoui <usftadlaoui@gmail.com>
2023-12-12 14:18:39 +0100

    validation login && navbar change color

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -10,7 +10,7 @@ const header = () => {
   return (
       <>
               
-          <nav className="bg-gray-800">
+          <nav className="bg-blue-400 ">
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
           <div className="relative flex h-16 items-center justify-between">
             <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
@@ -28,21 +28,21 @@ const header = () => {
               </button>
             </div>
             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-              <div className="flex flex-shrink-0 items-center">
+              {/* <div className="flex flex-shrink-0 items-center">
                  <img className="h-8 w-auto" src={loginImage} alt="Your Company" />
-              </div>
+              </div> */}
               <div className="hidden sm:ml-6 sm:block">
                 <div className="flex space-x-4">
                 
-                  <a href="#" className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Dashboard</a>
-                  <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Team</a>
-                  <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Projects</a>
-                  <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Calendar</a>
+                  <a href="#" className=" text-white text-400 hover:text-black rounded-md px-3 py-2 text-sm font-medium " aria-current="page">Dashboard</a>
+                  <a href="#" className="text-white text-400 hover:text-black rounded-md px-3 py-2 text-sm font-medium">Team</a>
+                  <a href="#" className="text-white text-400 hover:text-black rounded-md px-3 py-2 text-sm font-medium">Projects</a>
+                  <a href="#" className="text-white text-400 hover:text-black rounded-md px-3 py-2 text-sm font-medium">Calendar</a>
                 </div>
               </div>
             </div>
             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-              <button type="button" className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+              <button type="button" className="relative rounded-full bg-blue-400 p-1 text-400 hover:text-black text-white">
                 <span className="absolute -inset-1.5"></span>
                 <span className="sr-only">View notifications</span>
                 <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,68 +1,103 @@
-import React from 'react'
+import React from 'react';
+import { useForm, Controller } from 'react-hook-form';
 import loginImage from '../assets/images/login.jpg';
-const login = () => {
-      const backgroundStyle = {
-        backgroundImage: `url(${loginImage})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
+
+const Login = () => {
+
+
+  const backgroundStyle = {
+    backgroundImage: `url(${loginImage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  };
+  const { handleSubmit, control, formState: {errors}, getValues} = useForm();
   
-      };
-  return (
-  <>
-    <div className="flex items-center min-h-screen p-4 bg-gray-100 lg:justify-center">
-      <div className="flex flex-col overflow-hidden bg-white rounded-md shadow-lg max md:flex-row md:flex-1 lg:max-w-screen-md" >
-        <div className="p-4 py-6 text-white bg-blue-500 md:w-80 md:flex-shrink-0 md:flex md:flex-col md:items-center md:justify-evenly" style={backgroundStyle}>
-          
-        </div>
-        <div className="p-5 bg-white md:flex-1">
-          <h3 className="my-4 text-2xl font-semibold text-gray-700">Account Login</h3>
-          <form action="#" className="flex flex-col space-y-5">
-            <div className="flex flex-col space-y-1">
-              <label htmlFor="email" className="text-sm font-semibold text-gray-500">Email address</label>
-              <input
-                type="email"
-                id="email"
-                autoFocus
-                className="px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200"
-              />
-            </div>
-            <div className="flex flex-col space-y-1">
-              <div className="flex items-center justify-between">
-                <label htmlFor="password" className="text-sm font-semibold text-gray-500">Password</label>
-                <a href="#" className="text-sm text-blue-600 hover:underline focus:text-blue-800">Forgot Password?</a>
-              </div>
-              <input
-                type="password"
-                id="password"
-                className="px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200"
-              />
-            </div>
-            <div className="flex items-center space-x-2">
-              <input
-                type="checkbox"
-                id="remember"
-                className="w-4 h-4 transition duration-300 rounded focus:ring-2 focus:ring-offset-0 focus:outline-none focus:ring-blue-200"
-              />
-              <label htmlFor="remember" className="text-sm font-semibold text-gray-500">Remember me</label>
-            </div>
-            <div>
-              <button
-                type="submit"
-                className="w-full px-4 py-2 text-lg font-semibold text-white transition-colors duration-300 bg-blue-500 rounded-md shadow hover:bg-blue-600 focus:outline-none focus:ring-blue-200 focus:ring-4"
-              >
-                Log in
-              </button>
-            </div>
-          </form>
-          <div class="text-sm flex justify-between items-center mt-3">
-          <p>If you don't have an account...</p>
-          <button class="py-2 px-5 ml-3 bg-white border rounded-xl hover:scale-110 duration-300 border-blue-400  ">Register</button>
-        </div>
-        </div>
-      </div>
-    </div>
-  </>
-  )
+   const onSubmit = (data) => {
+     console.log(data);
 }
 
-export default login
+  return (
+    <>
+      <div className="flex items-center min-h-screen p-4 bg-gray-100 lg:justify-center">
+        <div className="flex flex-col overflow-hidden bg-white rounded-md shadow-lg max md:flex-row md:flex-1 lg:max-w-screen-md">
+          <div
+            className="p-4 py-6 text-white bg-blue-500 md:w-80 md:flex-shrink-0 md:flex md:flex-col md:items-center md:justify-evenly"
+            style={backgroundStyle}
+          ></div>
+          <div className="p-5 bg-white md:flex-1">
+            <h3 className="my-4 text-2xl font-semibold text-gray-700">Account Login</h3>
+            <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col space-y-5">
+            <div className="flex flex-col space-y-1">
+                <label htmlFor="email" className="text-sm font-semibold text-gray-500">Email address</label>
+                <Controller
+                  name="email"
+                  control={control}
+                  rules={{ required: 'Email is required', pattern: /^\S+@\S+$/i }}
+                  render={({ field }) => (
+                    <>
+                      <input
+                        {...field}
+                        type="email"
+                        id="email"
+                        className={`px-4 py-2 transition duration-300 border ${errors.email ? 'border-red-500' : 'border-gray-300'} rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200`}
+                      />
+                      {errors.email && <p className="text-sm text-red-500">{errors.email.message}</p>}
+                    </>
+                  )}
+                />
+              </div>
+              <div className="flex flex-col space-y-1">
+                <label htmlFor="password" className="text-sm font-semibold text-gray-500">Password</label>
+                <Controller
+                  name="password"
+                  control={control}
+                  rules={{ required: 'Password is required', minLength: { value: 6, message: 'Password must be at least 6 characters' } }}
+                  render={({ field }) => (
+                    <>
+                      <input
+                        {...field}
+                        type="password"
+                        id="password"
+                        className={`px-4 py-2 transition duration-300 border ${errors.password ? 'border-red-500' : 'border-gray-300'} rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200`}
+                      />
+                      {errors.password && <p className="text-sm text-red-500">{errors.password.message}</p>}
+                    </>
+                  )}
+                />
+              </div>
+              <div className="flex items-center space-x-2">
+                <input
+                  type="checkbox"
+                  id="remember"
+                  className="w-4 h-4 transition duration-300 rounded focus:ring-2 focus:ring-offset-0 focus:outline-none focus:ring-blue-200"
+                />
+                <label htmlFor="remember" className="text-sm font-semibold text-gray-500">
+                  Remember me
+                </label>
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  className="w-full px-4 py-2 text-lg font-semibold text-white transition-colors duration-300 bg-blue-500 rounded-md shadow hover:bg-blue-600 focus:outline-none focus:ring-blue-200 focus:ring-4"
+                >
+                  Log in
+                </button>
+              </div>
+            </form>
+            <div className="text-sm flex justify-between items-center mt-3">
+              <p>If you don't have an account...</p>
+              <button
+                type='submit'
+                className="py-2 px-5 ml-3 bg-white border rounded-xl hover:scale-110 duration-300 border-blue-400"
+              >
+                Register
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
