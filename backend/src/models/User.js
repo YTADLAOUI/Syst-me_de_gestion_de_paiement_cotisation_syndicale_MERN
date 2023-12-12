b3eea213a8710ee9f8d9dc10645a3a1775f71d5d
youssef tadlaoui <usftadlaoui@gmail.com>
2023-12-12 16:16:51 +0100

    add Role model

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -16,7 +16,19 @@ const mongoose  = require("mongoose");
     password:{
       type:String,
       require:true,
+    },  
+    role:{
+      type: mongoose.Schema.Types.ObjectId,
+      ref:'roles'
     },
+    isEmailVerfied:{
+      type:Boolean,
+      require:true
+    },
+    isDeleted:{
+      type:Boolean,
+      require:true
+    }
   },{timestamps:true});
   
   const userModel=mongoose.model("Users",userSchema);
